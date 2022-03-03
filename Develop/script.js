8b0cc88a27870facc4a97bbd49aef1295179ae29
yuval7994 <yuvalba45@gmail.com>
2022-03-03 13:14:39 -0600

    starting the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,40 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz"; 
+const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+const numberCharacters = "1234567890";
+const specualCharacters = "!@#$%^&*()";
 
+// Functions
+function writePassword() {
+  // generates the password
+  var password = generatePassword();
+  // target the appropriate part of the HTML
+  var passwordText = document.querySelector("#password");
+  // write the password to the webpage 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// function generate our password
+function generatePassword () {
+
+
+// asks the user for password length 
+// makes sure the user provided a valid length 
+
+
+// ask the user for password criteria 
+// make sure we have at least one criteria 
+
+//random select from pool of characters to generate the password 
+
+
+}
+
+
+// Non function code 
 generateBtn.addEventListener("click", writePassword);
+
+
